f1dccd8df39aeb0d5d787ff74bdf31e18cf0ccf8
George Shiangoli <shiangolig@yahoo.co.uk>
2016-02-29 11:23:47 +0000

    New case clause added to getInstruction method to accommodate subtraction instruction recently created.

--- cw-one/src/sml/Translator.java
+++ cw-one/src/sml/Translator.java
@@ -99,6 +99,11 @@ public class Translator {
                 r = scanInt();
                 x = scanInt();
                 return new LinInstruction(label, r, x);
+            case "sub":
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new SubInstruction(label, r, s1, s2);
         }
 
         // You will have to write code here for the other instructions.
