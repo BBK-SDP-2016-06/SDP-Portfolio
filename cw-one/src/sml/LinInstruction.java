fff4cd59e099dcdebdd03b6235da70138697c08f
George Shiangoli <shiangolig@yahoo.co.uk>
2016-02-29 17:04:12 +0000

    Javadoc comments added

--- cw-one/src/sml/LinInstruction.java
+++ cw-one/src/sml/LinInstruction.java
@@ -1,15 +1,30 @@
 package sml;
 
 /**
- * This class ....
+ * This class represents the "lin" instruction as interpreted by the Simple
+ * Machine Language (SML). The instruction is of the format:
+ * ID lin R x
+ * where the ID is the line identifier within the program, R is the register
+ * that the result will be stored in, and x is the value to be stored.
  *
- * @author someone
+ * This class extends the abstract class Instruction.
+ *
+ * @author George Shiangoli
  */
-
 public class LinInstruction extends Instruction {
     private int register;
     private int value;
 
+    /**
+     * The primary constructor of this class calls the constructor of the extended
+     * abstract class Instruction to initially set the label and opcode parameters.
+     * The constructor also initialises the local fields register and value.
+     *
+     * @param label the label of the instruction. This is an identifier that can take the
+     *              form of any sequence of non-whitespace characters.
+     * @param register the target register that the value will be stored in.
+     * @param value the integer that is to be stored in the register.
+     */
     public LinInstruction(String label, int register, int value) {
         super(label, "lin");
         this.register = register;
@@ -17,11 +32,24 @@ public class LinInstruction extends Instruction {
 
     }
 
+    /**
+     * Executes this Instruction on machine m. The instruction simply stores the value in
+     * the register.
+     *
+     * @param m the machine that this instruction gets executed upon.
+     */
     @Override
     public void execute(Machine m) {
         m.getRegisters().setRegister(register, value);
     }
 
+    /**
+     * Determines the format of the println representation of this instruction. In addition
+     * to the string created by this method's call to the superclass, it also appends information
+     * about the register and the register and the value now stored within it.
+     *
+     * @return a string of the format "label: opcode register value is x".
+     */
     @Override
     public String toString() {
         return super.toString() + " register " + register + " value is " + value;
