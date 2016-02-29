d4a0baea9bef675c3fae388016e9f7fcff37637b
George Shiangoli <shiangolig@yahoo.co.uk>
2016-02-29 16:45:02 +0000

    Javadoc comments created for AddInstruction class.

--- cw-one/src/sml/AddInstruction.java
+++ cw-one/src/sml/AddInstruction.java
@@ -1,9 +1,16 @@
 package sml;
 
 /**
- * This class ....
+ * This class represents the "add" instruction as interpreted by the Simple
+ * Machine Language (SML). The instruction is of the format:
+ * ID add R O1 O2
+ * where the ID is the line identifier within the program, R is the register
+ * that the result will be stored in, and O1 and O2 hold the operands that will
+ * be involved in the operation.
  *
- * @author someone
+ * This class extends the abstract class Instruction.
+ *
+ * @author George Shiangoli
  */
 
 public class AddInstruction extends Instruction {
@@ -12,6 +19,20 @@ public class AddInstruction extends Instruction {
     private int op1;
     private int op2;
 
+    /**
+     * The primary constructor of this class calls the constructor of the extended
+     * abstract class Instruction to initially set the label and opcode parameters.
+     * The constructor also initialises the local fields result, op1 and op2.
+     *
+     * @param label the label of the instruction. This is an identifier that can take the
+     *              form of any sequence of non-whitespace characters.
+     * @param result the target register that the resulting computation of this instruction
+     *               will be stored.
+     * @param op1 the first register whose value will be involved in the computation of this
+     *            instruction.
+     * @param op2 the second register whose value will be involved in the computation of this
+     *            instruction.
+     */
     public AddInstruction(String label, int result, int op1, int op2) {
         super(label, "add");
         this.result = result;
@@ -19,6 +40,11 @@ public class AddInstruction extends Instruction {
         this.op2 = op2;
     }
 
+    /**
+     * Executes this Instruction on machine m. The instruction adds the values found in
+     * registers op1 and op2
+     * @param m the machine that this instruction gets executed upon.
+     */
     @Override
     public void execute(Machine m) {
         int value1 = m.getRegisters().getRegister(op1);
@@ -26,6 +52,15 @@ public class AddInstruction extends Instruction {
         m.getRegisters().setRegister(result, value1 + value2);
     }
 
+    /**
+     * Determines the format of the println representation of this instruction. In addition
+     * to the string created by this method's call to the superclass, it also appends information
+     * on the addition taking place, the registers involved and the register that the result gets stored
+     * in.
+     *
+     * @return a string of the format "label: opcode op1 + op2 to result" of this instruction. This
+     * string subsequently gets built upon by subclasses extending this superclass.
+     */
     @Override
     public String toString() {
         return super.toString() + " " + op1 + " + " + op2 + " to " + result;
