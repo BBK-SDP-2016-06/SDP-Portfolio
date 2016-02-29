fd5e4b668c4c02e0d7cc5ad2f8334f4c4534124b
George Shiangoli <shiangolig@yahoo.co.uk>
2016-02-29 16:33:44 +0000

    Javadoc comments added to Instruction abstract class.

--- cw-one/src/sml/Instruction.java
+++ cw-one/src/sml/Instruction.java
@@ -1,31 +1,47 @@
 package sml;
 
 /**
- * This class is the superclass of the classes for machine instructions
+ * This class is the superclass of the classes for machine instructions. It holds the
+ * fields that are common across all instructions that are implemented by the Simple
+ * Machine Language (SML): a label and an opcode.
  *
- * @author someone
+ * @author George Shiangoli
  */
 
 public abstract class Instruction {
+
     protected String label;
     protected String opcode;
 
-    // Constructor: an instruction with label l and opcode op
-    // (op must be an operation of the language)
-
+    /**
+     * The primary constructor that gets called by all instruction subclasses. It
+     * instantiates both the label and opcode of each Instruction.
+     *
+     * @param l the label of the instruction. This is an identifier that can take the
+     *          form of any sequence of non-whitespace characters.
+     * @param op the opcode of the instruction, currently limited to "add", "sub", "mul",
+     *           "div", "out", "lin" and "bnz" i.e. the abbreviated terms of all instructions
+     *           accepted the SML.
+     */
     public Instruction(String l, String op) {
         this.label = l;
         this.opcode = op;
     }
 
-    // = the representation "label: opcode" of this Instruction
-
+    /**
+     * Determines the format of the println representation of this and all implemented subclasses.
+     *
+     * @return a string of the format "label: opcode" of this instruction. This string subsequently
+     * gets built upon by subclasses extending this superclass.
+     */
     @Override
     public String toString() {
         return label + ": " + opcode;
     }
 
-    // Execute this instruction on machine m.
-
+    /**
+     * Executes this Instruction on machine m.
+     * @param m the machine that this instruction gets executed upon.
+     */
     public abstract void execute(Machine m);
 }
