1125f67d774cfe8821faeee14c5d1f4b94b4379c
George Shiangoli <shiangolig@yahoo.co.uk>
2016-03-23 14:08:35 +0000

    Solution to question 16 completed.

--- exercises/week08/Functional continued (Optional)/src/part3_sequences/question16/FlattenList.scala
+++ exercises/week08/Functional continued (Optional)/src/part3_sequences/question16/FlattenList.scala
@@ -5,4 +5,7 @@ object FlattenList extends App {
   def flatten[A](xs: List[List[A]]): List[A] =
     (xs :\ (Nil: List[A])) {(x, xs) => x ::: xs}
 
+  def flatten2[A](xs: List[List[A]]): List[A] =
+    ((Nil: List[A]) /: xs)((xs, x) => xs ::: x)
+
 }
